26e8e84d6766fa78e61c4217e1b983a3c5dc572d
lkm981324 <matthew6303@gmail.com>
2019-06-10 14:09:57 +0800

    Updates

--- precache-manifest.64c40f7b9935954af0f6298571479d40.js
+++ precache-manifest.64c40f7b9935954af0f6298571479d40.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd998b3ac606d6d1bc9266bb28a03434",
+    "revision": "0f238955e6899522ee670cfe23e5e2f1",
     "url": "/matthew-lee/index.html"
   },
   {
-    "revision": "bb450c76ff38627a0a84",
-    "url": "/matthew-lee/static/js/2.50122ce7.chunk.js"
+    "revision": "dc4821b10c55a945275c",
+    "url": "/matthew-lee/static/js/2.4c134487.chunk.js"
   },
   {
-    "revision": "831c973d108fff44c384",
-    "url": "/matthew-lee/static/js/main.ebae33a9.chunk.js"
+    "revision": "a3f64b1604c1531379d8",
+    "url": "/matthew-lee/static/js/main.abebe0d9.chunk.js"
   },
   {
     "revision": "d8d1c54d9dfb5dfc559a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/matthew-lee/precache-manifest.da441a584bee6f21853834dbd2f517b2.js"
+  "/matthew-lee/precache-manifest.64c40f7b9935954af0f6298571479d40.js"
 );
 
 self.addEventListener('message', (event) => {
